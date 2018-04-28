71c1e0334e9c6916bb83813571f586f586bacf8b
Babak Yakhchali <babak.yakhchali@gmail.com>
2018-04-28 12:00:14 +0430

    added whole directory loading using wget and xml curl endpoint

--- src/controllers/XmlCurlController.ts
+++ src/controllers/XmlCurlController.ts
@@ -9,10 +9,17 @@ export class XmlCurlController {
     constructor(){
         this.fsDomainController = new FsDomainController();
     }
+    
 
     @Get("/directory")
     @Render("xmlcurl/directory.pug")
     async directory() {     
+        let domains = await this.fsDomainController.getAll();    
+        return {domains:domains};
+    }
+    @Get("/directory/section")
+    @Render("xmlcurl/directory-section.pug")
+    async directorySection() {     
         let domains = await this.fsDomainController.getAll();    
         return {domains:domains};
     }
